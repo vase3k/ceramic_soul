6354a08178910f811c85f27b40859970693c21e8
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-18 11:16:11 +0300

    slider half way done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,34 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import '/src/sass/style.scss';
+
+try {
+    const swiper = new Swiper('.works__swiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 35,
+            },
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) {
+    console.log(e);
+}
