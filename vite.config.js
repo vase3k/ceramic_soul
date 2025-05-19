65fa710358e2b7d9aab65b9202217158c8369b77
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-19 09:04:33 +0300

    testing deployement

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const removeCrossorigin = () => {
+    return {
+        name: 'remove-crossorigin',
+        transformIndexHtml(html) {
+            return html.replace(/crossorigin/g, '');
+        },
+    };
+};
+
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-			},
-		},
-	},
+    base: '',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
+    plugins: [removeCrossorigin()],
 });
