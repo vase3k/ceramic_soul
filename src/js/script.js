6348f426575cf0b9339e985cb5e6b9ac14bf9867
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-20 15:26:12 +0300

    burder menu javascript done

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,24 @@
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import '/src/sass/style.scss';
+
+const burger = document.querySelector('.burger'),
+    close = document.querySelector('.header__menu-close'),
+    menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.add('header__menu_active');
+    document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('header__menu_active');
+    document.body.style.overflow = '';
+});
 
 try {
     const swiper = new Swiper('.works__slider', {
