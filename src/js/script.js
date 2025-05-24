791c9c3be6537be8bee0b815b20598e983dfd6ea
Vasiliy Vinogradov <vase3k@gmail.com>
2025-05-24 20:12:40 +0300

    form fetch function

--- src/js/script.js
+++ src/js/script.js
@@ -24,40 +24,36 @@ try {
 } catch (e) {}
 
 try {
-    const touch__form = new JustValidate(
-        document.querySelector('.touch__form'),
-        { submitFormAutomatically: true },
-        [
-            {
-                key: 'enter your name',
-                dict: {
-                    english: 'enter your name',
-                    russian: 'введите имя ебта',
-                },
+    const touch__form = new JustValidate(document.querySelector('.touch__form'), undefined, [
+        {
+            key: 'enter your name',
+            dict: {
+                english: 'enter your name',
+                russian: 'введите имя ебта',
             },
-            {
-                key: 'enter your email',
-                dict: {
-                    english: 'enter your email',
-                    russian: 'введите мыло ваще',
-                },
+        },
+        {
+            key: 'enter your email',
+            dict: {
+                english: 'enter your email',
+                russian: 'введите мыло ваще',
             },
-            {
-                key: 'please fill up questioner',
-                dict: {
-                    english: 'please fill up questioner',
-                    russian: 'заполни поле чувак',
-                },
+        },
+        {
+            key: 'please fill up questioner',
+            dict: {
+                english: 'please fill up questioner',
+                russian: 'заполни поле чувак',
             },
-            {
-                key: 'you forgot the checkbox',
-                dict: {
-                    english: 'you forgot the checkbox',
-                    russian: 'забыл кликнуть мудила',
-                },
+        },
+        {
+            key: 'you forgot the checkbox',
+            dict: {
+                english: 'you forgot the checkbox',
+                russian: 'забыл кликнуть мудила',
             },
-        ]
-    );
+        },
+    ]);
     touch__form
         .addField('#name', [
             {
@@ -112,11 +108,24 @@ try {
                     )[0],
             }
         )
+        .onSuccess((e) => {
+            const form = e.currentTarget;
+            const formData = new FormData(form);
+            fetch('https://httpbin.org/post', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log('succes', data);
+                    form.reset();
+                });
+        })
         .setCurrentLocale('russian');
 } catch (e) {}
 
 try {
-    const footer__form = new JustValidate('#footer__form', { submitFormAutomatically: true }, [
+    const footer__form = new JustValidate('#footer__form', undefined, [
         {
             key: 'enter your email',
             dict: {
@@ -128,6 +137,11 @@ try {
                 english: 'you forgot the checkbox',
                 russian: 'забыл кликнуть мудила',
             },
+            key: 'this is not an email',
+            dict: {
+                english: 'this is not an email',
+                russian: 'це не емайл лошара',
+            },
         },
     ]);
     footer__form
@@ -137,6 +151,10 @@ try {
                 {
                     rule: 'required',
                     errorMessage: 'enter your email',
+                },
+                {
+                    rule: 'email',
+                    errorMessage: 'this is not an email',
                 },
             ],
             {
@@ -155,6 +173,19 @@ try {
                 errorsContainer: document.querySelector('.footer-checkbox-error-message'),
             }
         )
+        .onSuccess((event) => {
+            const form = event.currentTarget,
+                formData = new FormData(form);
+            fetch('https://httpbin.org/post', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log('succes', data);
+                    form.reset();
+                });
+        })
         .setCurrentLocale('russian');
 } catch (e) {}
 
