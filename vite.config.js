beab21d88e51cb17bd771a9ef52e8de945465019
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-08 12:02:47 +0300

    image compression

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import createFoldersPlugin from './src/js/createFolder';
+import viteImagemin from 'vite-plugin-imagemin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -17,5 +18,34 @@ export default defineConfig({
             },
         },
     },
-    plugins: [createFoldersPlugin()],
+    plugins: [
+        createFoldersPlugin(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            mozjpeg: {
+                quality: 70,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
 });
